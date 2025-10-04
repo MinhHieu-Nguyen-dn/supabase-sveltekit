371cc8805c66efd399ec84b956bdb5b39db83825
MinhHieu-Nguyen-dn <mhieu.forwork@gmail.com>
2025-10-04 13:17:49 +0700

    feat: init client on the server; update interface for Locals & PageData

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,16 @@
+import { SupabaseClient, Session } from '@supabse/supabase-js'
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient
+			safeGetSession(): Promise<{ session: Session | null; user?: Session["user"] | null }>
+		}
+		interface PageData {
+			session: Session | null
+			user?: Session["user"] | null
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
